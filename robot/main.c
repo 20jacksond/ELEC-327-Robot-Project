27c07cc511ca4a29118e38094c258d234654bf97
20jacksond <dlj3@rice.edu>
2023-05-02 23:12:47 -0500

    Added low power mode handling and PWM clock setup.

--- robot/main.c
+++ robot/main.c
@@ -13,8 +13,18 @@ int main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;   // Stop WDT
 
+    //setup PWM (next few lines from previous labs)
+    if (CALBC1_1MHZ == 0xFF || CALDCO_1MHZ == 0xff)
+        while(1); // Erased calibration data? Trap!
+
+    BCSCTL1 = CALBC1_1MHZ; // Set the DCO to 1 MHz
+    DCOCTL = CALDCO_1MHZ; // And load calibration data
+
     // Input ports (add P1.1 later)
-    P1DIR |= BIT0 + BIT1;  // Configure P1.0 and P1.1 as input ports (P1.0 for moving, P1.1 for turning)
+    //these will be used to interface with the vision processing commands from the BeagleBone
+    P1DIR &= ~(BIT0 + BIT1);  // Configure P1.0 and P1.1 as input ports (P1.0 for moving, P1.1 for turning)
+    P1SEL = 0;
+    P1SEL2 = 0;
     P1REN |= BIT0 + BIT1;    // Enable pull-up/down resistors on P1.0 and P1.1
     P1OUT |= BIT0 + BIT1;  // Set pull-up resistors on P1.0 and P1.1
 
@@ -28,16 +38,18 @@ int main(void)
 
     // Output ports (for controller and servo)
     P2DIR |= BIT5 + BIT3 + BIT1 + BIT0; // We need 2.5, 2.1, and 2.0 connected!
+    //2.1 is used for sleeping/waking the microcontroller, pin 2.0 is used for the motor controller control signal,
+    //and 2.5 is used for the servo PWM.
 
     P2SEL = 0;
     P2SEL2 = 0;
 
 
     while (1) {
-        __bis_SR_register(LPM3_bits + GIE);       // Enter LPM3 w/ interrupt
+        //CPU is not placed in sleep mode because we must keep the motor controller awake
         switch (move){
             case 0: // Forward State
-                P2OUT ^= BIT0;  // Move Forward
+                P2OUT |= (BIT0 + BIT1);  // Move Forward (make sure motor controller is awake)
                 __delay_cycles(DELAY);
                 if ((P1IN & BIT0)){ // Check if P1.0 is high
                     move = 1;
@@ -45,7 +57,7 @@ int main(void)
                 }
                 break;
             case 1: // IDLE State
-                P2OUT &= ~BIT0;  // Stop Moving
+                P2OUT &= ~(BIT0 + BIT1);  // Stop Moving (also sends motor controller to sleep for low power mode)
                 switch (turn){
                         case 0: // Turn left, need to add more stuff in here
                             TA0CCR1 = LEFT;
